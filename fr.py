ffa22fa0e32014fdbf5fdd17af12eaa9ee89c50d
Adil shagoo <60344586+adilshagoo87@users.noreply.github.com>
2020-01-27 16:58:44 +0530

    Add files via upload

---
+++ fr.py
@@ -0,0 +1,57 @@
+import cv2
+import numpy as np
+import pafy
+url = 'https://www.youtube.com/watch?v=1XIOmKGjgho'
+vPafy = pafy.new(url)
+play = vPafy.getbest(preftype="mp4")
+cap = cv2.VideoCapture(0)
+
+cap.set(3, 480) #set width of the frame
+cap.set(4, 640) #set height of the frame
+
+MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
+
+age_list = ['(0, 2)', '(4, 6)', '(8, 12)', '(15, 20)', '(25, 32)', '(38, 43)', '(48, 53)', '(60, 100)']
+gender_list = ['Male', 'Female']
+def load_caffe_models():
+ 
+    age_net = cv2.dnn.readNetFromCaffe('deploy_age.prototxt', 'age_net.caffemodel')
+
+    gender_net = cv2.dnn.readNetFromCaffe('deploy_gender.prototxt', 'gender_net.caffemodel')
+
+    return(age_net, gender_net)
+def video_detector(age_net, gender_net):
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    while True:
+        ret, image = cap.read()
+        face_cascade = cv2.CascadeClassifier('/home/user4/Documents/facial recognition/harr/haarcascade_frontalface_alt.xml')
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.1, 5)
+        if(len(faces)>0):
+            print("Found {} faces".format(str(len(faces))))
+        for (x, y, w, h )in faces:
+            cv2.rectangle(image, (x, y), (x+w, y+h), (255, 255, 0), 2)
+            face_img = image[y:y+h, h:h+w].copy()
+            blob = cv2.dnn.blobFromImage(face_img, 1, (227, 227), MODEL_MEAN_VALUES, swapRB=False)
+            gender_net.setInput(blob)
+            gender_preds = gender_net.forward()
+            gender = gender_list[gender_preds[0].argmax()]
+            print("Gender : " + gender)
+            age_net.setInput(blob)
+            age_preds = age_net.forward()
+            age = age_list[age_preds[0].argmax()]
+            print("Age Range: " + age)
+            overlay_text = "%s %s" % (gender, age)
+            cv2.putText(image, overlay_text, (x, y), font, 1, (255, 255, 255), 2, cv2.LINE_AA)
+            cv2.imshow('frame', image)
+            if cv2.waitKey(1) & 0xFF == ord('q'): 
+                break
+cap.release()
+cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    age_net, gender_net = load_caffe_models()
+
+    video_detector(age_net, gender_net)
+    video_detector(age_net, gender_net)
+
